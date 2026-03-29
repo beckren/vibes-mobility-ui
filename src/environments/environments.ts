532ba12db120be90de089b743a34af925ad4f3c9
Rene Wolf <47030197+beckren@users.noreply.github.com>
2026-03-29 11:31:29 +0200

    Update API URL for production environment

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://vibes-mobility.onrender.com',
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://vibes-mobility.onrender.com',
+  //apiUrl: 'http://localhost:8080',
   devBypassAuth: false   // keep false so real auth is used
 };
